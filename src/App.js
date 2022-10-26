51c95556aca63a1cce24fb8255018f8810747d61
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-10-26 18:12:51 -0300

    Adiciona Preview da carta

--- src/App.js
+++ src/App.js
@@ -3,12 +3,71 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      'name-input': '',
+      'description-input': '',
+      'attr1-input': '',
+      'attr2-input': '',
+      'attr3-input': '',
+      'image-input': '',
+      'rare-input': 'normal',
+      'trunfo-input': false,
+    };
+  }
+
+  onInputChange = (event) => {
+    const id = event.target.getAttribute('id');
+
+    if (id === 'trunfo-input') {
+      this.setState({
+        [id]: event.target.checked,
+      });
+      return;
+    }
+
+    this.setState({
+      [id]: event.target.value,
+    });
+  };
+
   render() {
+    const {
+      'name-input': cardName,
+      'description-input': cardDescription,
+      'attr1-input': cardAttr1,
+      'attr2-input': cardAttr2,
+      'attr3-input': cardAttr3,
+      'image-input': cardImage,
+      'rare-input': cardRare,
+      'trunfo-input': cardTrunfo,
+    } = this.state;
+
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
